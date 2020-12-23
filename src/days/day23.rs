6ede41fcdd2b8a9f9864ca9986bb6e70b2cdacda
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2020-12-23 14:14:10 +0000

    removed heap allocated vecs

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -1,19 +1,21 @@
 use std::time::{Duration, Instant};
+use std::convert::TryInto;
 
-fn load_cups(mut input: usize) -> Vec<u8> {
-    let mut res = Vec::new();
+fn load_cups(mut input: usize) -> [u8; 9] {
+    let mut res = [0; 9];
 
+    let mut ind = 9;
     while input > 0 {
+        ind -= 1;
         let i = input % 10;
         input /= 10;
-        res.push(i as u8);
+        res[ind] = i as u8;
     }
-    res.reverse();
 
-    res
+    res.try_into().unwrap()
 }
 
-fn part1(cups: Vec<u8>, turns: usize) -> usize {
+fn part1(cups: &[u8], turns: usize) -> usize {
     let mut tiles = vec![0; 10];
     for (p, n) in cups.iter().skip(1).zip(cups.iter()) {
         tiles[*n as usize] = *p as usize;
@@ -26,29 +28,26 @@ fn part1(cups: Vec<u8>, turns: usize) -> usize {
 
     let mut curr = *cups.first().unwrap() as usize;
     for _ in 0..turns {
-        let mut next_vals = Vec::with_capacity(3);
-        let mut w = curr;
-        for _ in 0..3 {
-            w = tiles[w];
-            next_vals.push(w);
-        }
+        let a = tiles[curr];
+        let b = tiles[a];
+        let c = tiles[b];
 
         let mut destination = curr - 1;
         if destination == 0 {
             destination = 9;
         }
-        while next_vals.contains(&destination) {
+        while destination == a || destination == b || destination == c {
             destination -= 1;
             if destination == 0 {
                 destination = 9;
             }
         }
 
-        let after_curr = tiles[next_vals[2]];
-        tiles[curr] = after_curr;
+        let now_after_curr = tiles[c];
+        tiles[curr] = now_after_curr;
 
-        tiles[next_vals[2]] = tiles[destination];
-        tiles[destination] = next_vals[0];
+        tiles[c] = tiles[destination];
+        tiles[destination] = a;
         curr = tiles[curr];
     }
 
@@ -62,7 +61,7 @@ fn part1(cups: Vec<u8>, turns: usize) -> usize {
     res
 }
 
-fn part2(cups: Vec<u8>) -> usize {
+fn part2(cups: &[u8]) -> usize {
     let mut tiles = vec![0; 1_000_001];
     for (p, n) in cups.iter().skip(1).zip(cups.iter()) {
         tiles[*n as usize] = *p as usize;
@@ -84,29 +83,26 @@ fn part2(cups: Vec<u8>) -> usize {
 
     let mut curr = *cups.first().unwrap() as usize;
     for _ in 0..10_000_000 {
-        let mut next_vals = Vec::with_capacity(3);
-        let mut w = curr;
-        for _ in 0..3 {
-            w = tiles[w];
-            next_vals.push(w);
-        }
+        let a = tiles[curr];
+        let b = tiles[a];
+        let c = tiles[b];
 
         let mut destination = curr - 1;
         if destination == 0 {
             destination = 1_000_000;
         }
-        while next_vals.contains(&destination) {
+        while destination == a || destination == b || destination == c {
             destination -= 1;
             if destination == 0 {
                 destination = 1_000_000;
             }
         }
 
-        let after_curr = tiles[next_vals[2]];
-        tiles[curr] = after_curr;
+        let now_after_curr = tiles[c];
+        tiles[curr] = now_after_curr;
+        tiles[c] = tiles[destination];
+        tiles[destination] = a;
 
-        tiles[next_vals[2]] = tiles[destination];
-        tiles[destination] = next_vals[0];
         curr = tiles[curr];
     }
 
@@ -119,8 +115,8 @@ fn part2(cups: Vec<u8>) -> usize {
 pub fn run() -> (String, String, Duration) {
     let start = Instant::now();
     let cups = load_cups(219347865);
-    let p1 = part1(cups.clone(), 100);
-    let p2 = part2(cups);
+    let p1 = part1(&cups, 100);
+    let p2 = part2(&cups);
 
     (p1.to_string(), p2.to_string(), start.elapsed())
 }
@@ -132,13 +128,13 @@ mod tests {
     #[test]
     fn test_part1() {
         let cups = load_cups(389125467);
-        assert_eq!(part1(cups.clone(), 10), 92658374);
-        assert_eq!(part1(cups, 100), 67384529);
+        assert_eq!(part1(&cups, 10), 92658374);
+        assert_eq!(part1(&cups, 100), 67384529);
     }
 
     #[test]
     fn test_part2() {
         let cups = load_cups(389125467);
-        assert_eq!(part2(cups), 149245887792);
+        assert_eq!(part2(&cups), 149245887792);
     }
 }
